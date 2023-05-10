4520f3967f1f184994598c3ee61348e306a4e6ee
shiwei <1828045443@qq.com>
2023-05-10 16:31:33 +0800

    完成别名联想设置

--- vue3-rabbit/vite.config.js
+++ vue3-rabbit/vite.config.js
@@ -5,10 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue()],
+    resolve: {
+        // 实际的路径转化 @ -> src  alias : 别名虚拟
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
