7f1e3cde1f7a7fffefecad691a15031c0a70bed9
shiwei <1828045443@qq.com>
2023-05-10 16:57:55 +0800

    element主题色的定制

--- vue3-rabbit/vite.config.js
+++ vue3-rabbit/vite.config.js
@@ -18,13 +18,27 @@ export default defineConfig({
             resolvers: [ElementPlusResolver()],
         }),
         Components({
-            resolvers: [ElementPlusResolver()],
+            resolvers: [
+                // 1. 配置elementPlus采用sass样式配色系统
+                ElementPlusResolver({ importStyle: "sass" }),
+            ],
         }),
     ],
     resolve: {
-        // 实际的路径转化 @ -> src  alias : 别名虚拟
+        // 实际的路径转化 @ -> src  alias : 别名
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // 2. 自动导入定制化样式文件进行样式覆盖
+                additionalData: `
+          @use "@/styles/element/index.scss" as *;
+          @use "@/styles/var.scss" as *;
+        `,
+            }
         }
     }
 })
