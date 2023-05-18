061822fdc7d99883e08b62e115ba6a94f5378d6c
shiwei <1828045443@qq.com>
2023-05-18 11:59:07 +0800

    home-图片懒加载指令实现

--- vue3-rabbit/src/apis/home.js
+++ vue3-rabbit/src/apis/home.js
@@ -8,3 +8,19 @@ export function getBannerAPI() {
         url: '/home/banner'
     })
 }
+
+// 获取新鲜好物
+
+export const findNewAPI = () => {
+    return httpInstance({
+        url: '/home/new'
+    })
+}
+
+// 获取人气推荐
+
+export const getHotAPI = () => {
+    return httpInstance({
+        url: '/home/hot'
+    })
+}
--- vue3-rabbit/src/main.js
+++ vue3-rabbit/src/main.js
@@ -4,9 +4,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { useIntersectionObserver } from '@vueuse/core'
 
 // 引入初始化样式文件
 import '@/styles/common.scss'
+
 
 // 测试接口函数
 // import { getCategory } from '@/apis/testAPI'
@@ -22,3 +24,23 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// 定义全局指令
+
+app.directive('img-lazy', {
+    mounted(el, binding) {
+        // el: 指令绑定的那个元素 img
+        // binding: binding.value 指令等于号后面绑定的表达式的值 图片url
+        console.log(el, binding.value)
+        useIntersectionObserver(
+            el,
+            ([{ isIntersecting }]) => {
+                console.log(isIntersecting)
+                if (isIntersecting) {
+                    // 进入视图区域
+                    el.src = binding.value
+                }
+            },
+        )
+    }
+})
