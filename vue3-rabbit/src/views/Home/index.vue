72d99ccae59b996f69f43d6694f5f38d5f3ad885
shiwei <1828045443@qq.com>
2023-05-18 11:31:56 +0800

    home-面板组件封装

--- vue3-rabbit/src/views/Home/index.vue
+++ vue3-rabbit/src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,4 +15,15 @@ import HomeProduct from './components/HomeProduct.vue'
     <HomeNew />
     <HomeHot />
     <HomeProduct />
+    <!-- 测试面板组件 -->
+    <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+        <div>
+            我是新鲜好物的插槽
+        </div>
+    </HomePanel>
+    <HomePanel title="人气推荐" sub-title="人气推荐 好多商品">
+        <div>
+            我是人气推荐的插槽
+        </div>
+    </HomePanel>
 </template>
