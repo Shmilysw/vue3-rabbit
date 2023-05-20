cf1c4a2102fb6559a78b0fd37f775639be5bb35c
shiwei <1828045443@qq.com>
2023-05-20 17:58:44 +0800

    放大镜效果-滑块跟随鼠标移动

--- vue3-rabbit/src/components/ImageView/index.vue
+++ vue3-rabbit/src/components/ImageView/index.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { ref } from 'vue'
-// import { useMouseInElement } from '@vueuse/core'
+import { ref, watch } from 'vue'
+import { useMouseInElement } from '@vueuse/core'
 
 // 图片列表
 const imageList = [
@@ -18,55 +18,55 @@ const enterhandler = (i) => {
     activeIndex.value = i
 }
 
-// // 2. 获取鼠标相对位置
-// const target = ref(null)
-// const { elementX, elementY, isOutside } = useMouseInElement(target)
+// 2. 获取鼠标相对位置
+const target = ref(null)
+const { elementX, elementY, isOutside } = useMouseInElement(target)
 
-// // 3. 控制滑块跟随鼠标移动（监听elementX/Y变化，一旦变化 重新设置left/top）
-// const left = ref(0)
-// const top = ref(0)
+// 3. 控制滑块跟随鼠标移动（监听elementX/Y变化，一旦变化 重新设置left/top）
+const left = ref(0)
+const top = ref(0)
 
-// const positionX = ref(0)
-// const positionY = ref(0)
-// watch([elementX, elementY, isOutside], () => {
+const positionX = ref(0)
+const positionY = ref(0)
+watch([elementX, elementY, isOutside], () => {
 
-//   // 如果鼠标没有移入到盒子里面 直接不执行后面的逻辑
-//   if (isOutside.value) return
+    // 如果鼠标没有移入到盒子里面 直接不执行后面的逻辑
+    if (isOutside.value) return
 
-//   // 有效范围内控制滑块距离
-//   // 横向
-//   if (elementX.value > 100 && elementX.value < 300) {
-//     left.value = elementX.value - 100
-//   }
-//   // 纵向
-//   if (elementY.value > 100 && elementY.value < 300) {
-//     top.value = elementY.value - 100
-//   }
+    // 有效范围内控制滑块距离
+    // 横向
+    if (elementX.value > 100 && elementX.value < 300) {
+        left.value = elementX.value - 100
+    }
+    // 纵向
+    if (elementY.value > 100 && elementY.value < 300) {
+        top.value = elementY.value - 100
+    }
 
-//   // 处理边界
-//   if (elementX.value > 300) { left.value = 200 }
-//   if (elementX.value < 100) { left.value = 0 }
+    // 处理边界
+    if (elementX.value > 300) { left.value = 200 }
+    if (elementX.value < 100) { left.value = 0 }
 
-//   if (elementY.value > 300) { top.value = 200 }
-//   if (elementY.value < 100) { top.value = 0 }
+    if (elementY.value > 300) { top.value = 200 }
+    if (elementY.value < 100) { top.value = 0 }
 
-//   // 控制大图的显示
-//   positionX.value = -left.value * 2
-//   positionY.value = -top.value * 2
+    // 控制大图的显示
+    positionX.value = -left.value * 2
+    positionY.value = -top.value * 2
 
-// })
-
+})
 </script>
 
 
 <template>
+    <!-- {{ elementX }}, {{ elementY }}, {{ isOutside }} -->
     <div class="goods-image">
 
         <!-- 左侧大图-->
         <div class="middle" ref="target">
             <img :src="imageList[activeIndex]" alt="" />
             <!-- 蒙层小滑块 -->
-            <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+            <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
         </div>
         <!-- 小图列表 -->
         <ul class="small">
@@ -77,11 +77,11 @@ const enterhandler = (i) => {
         <!-- 放大镜大图 -->
         <div class="large" :style="[
             {
-                backgroundImage: `url(${imageList[0]})`,
-                backgroundPositionX: `0px`,
-                backgroundPositionY: `0px`,
+                backgroundImage: `url(${imageList[activeIndex]})`,
+                backgroundPositionX: `${positionX}px`,
+                backgroundPositionY: `${positionY}px`,
             },
-        ]" v-show="false"></div>
+        ]" v-show="!isOutside"></div>
     </div>
 </template>
 
