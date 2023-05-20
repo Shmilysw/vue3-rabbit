7cbcdf64579ac0a54e97f414f3fbc7dfd542d015
shiwei <1828045443@qq.com>
2023-05-20 18:17:35 +0800

    详情页-图片列表展示

--- vue3-rabbit/src/components/ImageView/index.vue
+++ vue3-rabbit/src/components/ImageView/index.vue
@@ -2,14 +2,23 @@
 import { ref, watch } from 'vue'
 import { useMouseInElement } from '@vueuse/core'
 
+// props适配图片列表
+
+defineProps({
+    imageList: {
+        type: Array,
+        default: () => []
+    }
+})
+
 // 图片列表
-const imageList = [
-    "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
-    "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
-    "https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg",
-    "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
-    "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
-]
+// const imageList = [
+//     "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
+//     "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
+//     "https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg",
+//     "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
+//     "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
+// ]
 
 // 1.小图切换大图显示
 const activeIndex = ref(0)
--- vue3-rabbit/src/views/Detail/index.vue
+++ vue3-rabbit/src/views/Detail/index.vue
@@ -92,7 +92,7 @@ onMounted(() => getGoods())
                     <div class="goods-info">
                         <div class="media">
                             <!-- 图片预览区 -->
-                            <ImageView />
+                            <ImageView :image-list="goods.mainPictures" />
                             <!-- 统计数量 -->
                             <ul class="goods-sales">
                                 <li>
