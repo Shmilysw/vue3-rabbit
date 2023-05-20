15ddda28c66e2f7270b6d9fabf28f21b45e66c8a
shiwei <1828045443@qq.com>
2023-05-20 15:25:11 +0800

    详情-整体业务认识和路由配置

--- vue3-rabbit/src/router/index.js
+++ vue3-rabbit/src/router/index.js
@@ -7,6 +7,7 @@ import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
+import Detail from '@/views/Detail/index.vue'
 
 
 const router = createRouter({
@@ -28,6 +29,10 @@ const router = createRouter({
                 {
                     path: 'category/sub/:id',
                     component: SubCategory
+                },
+                {
+                    path: 'detail/:id',
+                    component: Detail
                 }
             ]
         },
