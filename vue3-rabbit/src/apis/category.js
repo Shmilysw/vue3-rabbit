a89b4268e1f97cec2ef3577a1622d7881dad9227
shiwei <1828045443@qq.com>
2023-05-20 14:29:23 +0800

    二级分类-基础列表渲染

--- vue3-rabbit/src/apis/category.js
+++ vue3-rabbit/src/apis/category.js
@@ -23,3 +23,21 @@ export const getCategoryFilterAPI = (id) => {
         }
     })
 }
+
+/**
+ * @description: 获取导航数据
+ * @data { 
+     categoryId: 1005000 ,
+     page: 1,
+     pageSize: 20,
+     sortField: 'publishTime' | 'orderNum' | 'evaluateNum'
+   } 
+ * @return {*}
+ */
+export const getSubCategoryAPI = (data) => {
+    return request({
+        url: '/category/goods/temporary',
+        method: 'POST',
+        data
+    })
+}
--- vue3-rabbit/src/views/SubCategory/index.vue
+++ vue3-rabbit/src/views/SubCategory/index.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { getCategoryFilterAPI } from '@/apis/category'
+import { getCategoryFilterAPI, getSubCategoryAPI } from '@/apis/category'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import GoodsItem from '../Home/components/GoodsItem.vue'
 
 // 获取面包屑导航数据
 const categoryData = ref({})
@@ -11,8 +12,23 @@ const getCategoryData = async () => {
     const res = await getCategoryFilterAPI(route.params.id)
     categoryData.value = res.result
 }
-
 onMounted(() => getCategoryData())
+
+// 获取基础列表数据渲染
+const goodList = ref([])
+const reqData = ref({
+    categoryId: route.params.id,
+    page: 1,
+    pageSize: 20,
+    sortField: 'publishTime'
+})
+const getGoodList = async () => {
+    const res = await getSubCategoryAPI(reqData.value)
+    console.log(res);
+    goodList.value = res.result.items
+}
+onMounted(() => getGoodList())
+
 
 // import { onMounted, ref } from 'vue'
 // import { useRoute } from 'vue-router'
@@ -84,7 +100,7 @@ onMounted(() => getCategoryData())
             </el-tabs>
             <div class="body">
                 <!-- 商品列表-->
-                <!-- <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id" /> -->
+                <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id" />
             </div>
         </div>
     </div>
