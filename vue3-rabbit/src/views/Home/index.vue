ad793bb9cb53bd308fd008dce67f8e77fecb9c99
shiwei <1828045443@qq.com>
2023-05-21 13:40:38 +0800

    图片预览组件-props适配以及优化操作

--- vue3-rabbit/src/views/Home/index.vue
+++ vue3-rabbit/src/views/Home/index.vue
@@ -16,7 +16,7 @@ import HomePanel from './components/HomePanel.vue'
     <HomeHot />
     <HomeProduct />
     <!-- 测试面板组件 -->
-    <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+    <!-- <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
         <div>
             我是新鲜好物的插槽
         </div>
@@ -25,5 +25,5 @@ import HomePanel from './components/HomePanel.vue'
         <div>
             我是人气推荐的插槽
         </div>
-    </HomePanel>
+    </HomePanel> -->
 </template>
