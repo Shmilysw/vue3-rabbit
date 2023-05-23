9f582d63bf7d81e3aea24116c421eee2aa4476e1
shiwei <1828045443@qq.com>
2023-05-23 21:08:16 +0800

    登录-表单校验

--- vue3-rabbit/src/views/Login/index.vue
+++ vue3-rabbit/src/views/Login/index.vue
@@ -1,70 +1,94 @@
 <script setup>
 
-
 // 表单校验（账号名+密码）
 
-// import { ref } from 'vue'
+import { ref } from 'vue'
 
-// import { ElMessage } from 'element-plus'
-// import 'element-plus/theme-chalk/el-message.css'
-// import { useRouter } from 'vue-router'
+// 1. 准备表单对象
+const form = ref({
+    account: '',
+    password: ''
+})
 
-// import { useUserStore } from '@/stores/userStore'
+// 2. 准备规则对象
+const rules = {
+    account: [
+        { required: true, message: '用户名不能为空', trigger: 'blur' }
+    ],
+    password: [
+        { required: true, message: '密码不能为空', trigger: 'blur' },
+        { min: 6, max: 14, message: '密码长度为6-14个字符', trigger: 'blur' }
+    ]
+}
 
-// const userStore = useUserStore()
 
-// // 1. 准备表单对象
-// const form = ref({
-//     account: '18610848230',
-//     password: '123456',
-//     agree: true
-// })
 
-// // 2. 准备规则对象
-// const rules = {
-//     account: [
-//         { required: true, message: '用户名不能为空', trigger: 'blur' }
-//     ],
-//     password: [
-//         { required: true, message: '密码不能为空', trigger: 'blur' },
-//         { min: 6, max: 14, message: '密码长度为6-14个字符', trigger: 'blur' },
-//     ],
-//     agree: [
-//         {
-//             validator: (rule, value, callback) => {
-//                 console.log(value)
-//                 // 自定义校验逻辑
-//                 // 勾选就通过 不勾选就不通过
-//                 if (value) {
-//                     callback()
-//                 } else {
-//                     callback(new Error('请勾选协议'))
-//                 }
-//             }
-//         }
-//     ]
-// }
 
-// // 3. 获取form实例做统一校验
-// const formRef = ref(null)
-// const router = useRouter()
-// const doLogin = () => {
-//     const { account, password } = form.value
-//     // 调用实例方法
-//     formRef.value.validate(async (valid) => {
-//         // valid: 所有表单都通过校验  才为true
-//         console.log(valid)
-//         // 以valid做为判断条件 如果通过校验才执行登录逻辑
-//         if (valid) {
-//             // TODO LOGIN
-//             await userStore.getUserInfo({ account, password })
-//             // 1. 提示用户
-//             ElMessage({ type: 'success', message: '登录成功' })
-//             // 2. 跳转首页
-//             router.replace({ path: '/' })
-//         }
-//     })
-// }
+
+/*
+import { ref } from 'vue'
+
+import { ElMessage } from 'element-plus'
+import 'element-plus/theme-chalk/el-message.css'
+import { useRouter } from 'vue-router'
+
+import { useUserStore } from '@/stores/userStore'
+
+const userStore = useUserStore()
+
+// 1. 准备表单对象
+const form = ref({
+    account: '18610848230',
+    password: '123456',
+    agree: true
+})
+
+// 2. 准备规则对象
+const rules = {
+    account: [
+        { required: true, message: '用户名不能为空', trigger: 'blur' }
+    ],
+    password: [
+        { required: true, message: '密码不能为空', trigger: 'blur' },
+        { min: 6, max: 14, message: '密码长度为6-14个字符', trigger: 'blur' },
+    ],
+    agree: [
+        {
+            validator: (rule, value, callback) => {
+                console.log(value)
+                // 自定义校验逻辑
+                // 勾选就通过 不勾选就不通过
+                if (value) {
+                    callback()
+                } else {
+                    callback(new Error('请勾选协议'))
+                }
+            }
+        }
+    ]
+}
+
+// 3. 获取form实例做统一校验
+const formRef = ref(null)
+const router = useRouter()
+const doLogin = () => {
+    const { account, password } = form.value
+    // 调用实例方法
+    formRef.value.validate(async (valid) => {
+        // valid: 所有表单都通过校验  才为true
+        console.log(valid)
+        // 以valid做为判断条件 如果通过校验才执行登录逻辑
+        if (valid) {
+            // TODO LOGIN
+            await userStore.getUserInfo({ account, password })
+            // 1. 提示用户
+            ElMessage({ type: 'success', message: '登录成功' })
+            // 2. 跳转首页
+            router.replace({ path: '/' })
+        }
+    })
+}
+*/
 
 // 1. 用户名和密码 只需要通过简单的配置（看文档的方式 - 复杂功能通过多个不同组件拆解）
 // 2. 同意协议  自定义规则  validator:(rule,value,callback)=>{}
@@ -93,12 +117,12 @@
                 </nav>
                 <div class="account-box">
                     <div class="form">
-                        <el-form label-position="right" label-width="60px" status-icon>
-                            <el-form-item label="账户">
-                                <el-input />
+                        <el-form :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
+                            <el-form-item prop="account" label="账户">
+                                <el-input v-model="form.account" />
                             </el-form-item>
-                            <el-form-item label="密码">
-                                <el-input />
+                            <el-form-item prop="password" label="密码">
+                                <el-input v-model="form.password" />
                             </el-form-item>
                             <el-form-item label-width="22px">
                                 <el-checkbox size="large">
