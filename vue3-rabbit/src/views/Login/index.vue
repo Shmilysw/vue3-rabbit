b6f7eb6350ffef7429a4784ad36de6a6de102d9d
shiwei <1828045443@qq.com>
2023-05-23 21:27:41 +0800

    登录-表单校验-统一校验

--- vue3-rabbit/src/views/Login/index.vue
+++ vue3-rabbit/src/views/Login/index.vue
@@ -36,8 +36,25 @@ const rules = {
     ]
 }
 
+// 3. 获取form实例做统一校验
+const formRef = ref(null)
+const doLogin = () => {
+    // 调用实例方法
+    formRef.value.validate((valid) => {
+        // valid: 所有表单都通过校验 才为true
+        console.log(valid);
+        // 以valid做为判断条件 如果通过校验才执行登录逻辑
+        if (valid) {
+            // TODO LOGIN
+
+        }
+    })
+}
 
 
+// 1. 用户名和密码 只需要通过简单的配置（看文档的方式 - 复杂功能通过多个不同组件拆解）
+// 2. 同意协议  自定义规则  validator:(rule,value,callback)=>{}
+// 3. 统一校验  通过调用form实例的方法 validate -> true
 
 
 /* 模板
@@ -132,7 +149,8 @@ const doLogin = () => {
                 </nav>
                 <div class="account-box">
                     <div class="form">
-                        <el-form :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
+                        <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px"
+                            status-icon>
                             <el-form-item prop="account" label="账户">
                                 <el-input v-model="form.account" />
                             </el-form-item>
@@ -144,7 +162,7 @@ const doLogin = () => {
                                     我已同意隐私条款和服务条款
                                 </el-checkbox>
                             </el-form-item>
-                            <el-button size="large" class="subBtn">点击登录</el-button>
+                            <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
                         </el-form>
                     </div>
                 </div>
