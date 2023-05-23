6a2946812d014d057cec27c9a567e384fa63f9f4
shiwei <1828045443@qq.com>
2023-05-23 21:17:13 +0800

    登录-表单检验-自定义校验规则

--- vue3-rabbit/src/views/Login/index.vue
+++ vue3-rabbit/src/views/Login/index.vue
@@ -6,8 +6,9 @@ import { ref } from 'vue'
 
 // 1. 准备表单对象
 const form = ref({
-    account: '',
-    password: ''
+    account: '18610848230',
+    password: '123456',
+    agree: true
 })
 
 // 2. 准备规则对象
@@ -18,6 +19,20 @@ const rules = {
     password: [
         { required: true, message: '密码不能为空', trigger: 'blur' },
         { min: 6, max: 14, message: '密码长度为6-14个字符', trigger: 'blur' }
+    ],
+    agree: [
+        {
+            validator: (rule, value, callback) => {
+                console.log(value);
+                // 自定义校验逻辑
+                // 勾选就通过，不勾选就不通过
+                if (value) {
+                    callback()
+                } else {
+                    callback(new Error('请勾选协议!'))
+                }
+            }
+        }
     ]
 }
 
@@ -25,7 +40,7 @@ const rules = {
 
 
 
-/*
+/* 模板
 import { ref } from 'vue'
 
 import { ElMessage } from 'element-plus'
@@ -124,8 +139,8 @@ const doLogin = () => {
                             <el-form-item prop="password" label="密码">
                                 <el-input v-model="form.password" />
                             </el-form-item>
-                            <el-form-item label-width="22px">
-                                <el-checkbox size="large">
+                            <el-form-item prop="agree" label-width="22px">
+                                <el-checkbox size="large" v-model="form.agree">
                                     我已同意隐私条款和服务条款
                                 </el-checkbox>
                             </el-form-item>
