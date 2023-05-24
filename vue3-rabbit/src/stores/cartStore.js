747f447961e256beec186423028d28637cd9d834
shiwei <1828045443@qq.com>
2023-05-24 17:47:44 +0800

    本地购物车-添加购物车实现

---
+++ vue3-rabbit/src/stores/cartStore.js
@@ -0,0 +1,30 @@
+// 封装购物车模块
+
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useCartStore = defineStore('cart', () => {
+    // 1. 定义state - cartList
+    const cartList = ref([])
+    // 2. 定义action - addCart
+    const addCart = (goods) => {
+        // 添加购物车操作
+        // 已添加过 - count + 1
+        // 没有添加过 - 直接push
+        // 思路: 通过匹配传递过来的商品对象中的skuId能不能在cartList中找到，找到了就是添加过
+        const item = cartList.value.find((item) => goods.skuId === item.skuId)
+        if (item) {
+            // 找到了
+            item.count++
+        } else {
+            // 没找到
+            cartList.value.push(goods)
+        }
+    }
+    return {
+        cartList,
+        addCart
+    }
+}, {
+    persist: true,
+})
--- vue3-rabbit/src/views/Detail/index.vue
+++ vue3-rabbit/src/views/Detail/index.vue
@@ -1,8 +1,11 @@
 <script setup>
 import DetailHot from './components/DetailHot.vue'
 import { getDetail } from '@/apis/detail'
+import { ElMessage } from 'element-plus'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import { useCartStore } from '@/stores/cartStore'
+const cartStore = useCartStore()
 const goods = ref({})
 const route = useRoute()
 const getGoods = async () => {
@@ -12,10 +15,37 @@ const getGoods = async () => {
 onMounted(() => getGoods())
 
 // sku 规格被操作时
+let skuObj = {}
 const skuChange = (sku) => {
     console.log(sku);
+    skuObj = sku
 }
 
+// count
+const count = ref(1)
+const countChange = (count) => {
+    console.log(count);
+}
+
+// 添加购物车
+const addCart = () => {
+    if (skuObj.skuId) {
+        // 规则已经选择 触发action
+        cartStore.addCart({
+            id: goods.value.id, // 商品id
+            name: goods.value.name, // 商品名称
+            picture: goods.value.mainPictures[0], // 图片
+            price: goods.value.price, // 最新价格
+            count: count.value, // 商品数量
+            skuId: skuObj.skuId, // skuId
+            attrsText: skuObj.specsText, // 商品规格文本
+            selected: true // 商品是否选中
+        })
+    } else {
+        // 规则没有选择 提示用户
+        ElMessage.warning('请选择规格')
+    }
+}
 
 
 // import DetailHot from './components/DetailHot.vue'
@@ -150,7 +180,7 @@ const skuChange = (sku) => {
                             <el-input-number v-model="count" @change="countChange" />
                             <!-- 按钮组件 -->
                             <div>
-                                <el-button size="large" class="btn">
+                                <el-button size="large" class="btn" @click="addCart">
                                     加入购物车
                                 </el-button>
                             </div>
