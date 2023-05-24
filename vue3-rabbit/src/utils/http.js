315cddccd1054f9c6dd8834adfb5dc84361cc0b4
shiwei <1828045443@qq.com>
2023-05-24 16:21:01 +0800

    登录-token拦截器中携带

--- vue3-rabbit/src/utils/http.js
+++ vue3-rabbit/src/utils/http.js
@@ -2,6 +2,7 @@
 import axios from "axios";
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
+import { useUserStore } from "@/stores/user";
 
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -12,6 +13,13 @@ const httpInstance = axios.create({
 
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+    // 1. 从pinia里面获取token数据
+    const userStore = useUserStore()
+    // 2. 按照后端的要求拼接token数据
+    const token = userStore.userInfo.token
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
     return config
 }, e => Promise.reject(e))
 
