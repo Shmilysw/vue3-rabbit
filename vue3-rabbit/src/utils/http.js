df8813405f3dbd639ca5811942c1592e10e1fe22
shiwei <1828045443@qq.com>
2023-05-24 17:14:12 +0800

    401拦截处理

--- vue3-rabbit/src/utils/http.js
+++ vue3-rabbit/src/utils/http.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useUserStore } from "@/stores/user";
+import router from "@/router";
 
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -25,11 +26,20 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+    // 从pinia里面获取token数据
+    const userStore = useUserStore()
     // 统一错误提示
     ElMessage({
         type: 'warning',
         message: e.response.data.message
     })
+    // 401 token失效处理
+    // 1. 清除本地用户数据
+    // 2. 跳转到登录页
+    if (e.response.status == 401) {
+        userStore.clearUserInfo()
+        router.push('/login')
+    }
     return Promise.reject(e)
 })
 
