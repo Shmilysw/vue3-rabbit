00f2406dbd318b4f78a32d99922a6103c1a02bd4
shiwei <1828045443@qq.com>
2023-05-29 17:47:07 +0800

    退出登录时清除购物车数据

--- vue3-rabbit/src/stores/userStore.js
+++ vue3-rabbit/src/stores/userStore.js
@@ -3,8 +3,10 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { loginAPI } from '@/apis/user'
+import { useCartStore } from './cartStore'
 
 export const useUserStore = defineStore('user', () => {
+    const cartStore = useCartStore()
     // 1. 定义管理用户数据的state
     const userInfo = ref({})
     // 2. 定义获取接口数据的action函数
@@ -16,6 +18,8 @@ export const useUserStore = defineStore('user', () => {
     // 退出时清除用户信息
     const clearUserInfo = () => {
         userInfo.value = {}
+        // 执行清除购物车的action
+        cartStore.clearCart()
     }
 
     // 3. 以对象的格式把state和action return
