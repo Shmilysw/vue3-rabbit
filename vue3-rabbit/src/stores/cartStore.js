64e7cba32d9c19c677fe71408ab22256f1dbe11f
shiwei <1828045443@qq.com>
2023-05-29 17:24:02 +0800

    接口购物车-加入购物车实现

--- vue3-rabbit/src/stores/cartStore.js
+++ vue3-rabbit/src/stores/cartStore.js
@@ -2,33 +2,61 @@
 
 import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
+import { useUserStore } from './user'
+import { insertCartAPI, findNewCartListAPI, delCartAPI } from '@/apis/cart'
 
 export const useCartStore = defineStore('cart', () => {
+    const userStore = useUserStore()
+    const isLogin = computed(() => userStore.userInfo.token)
     // 1. 定义state - cartList
     const cartList = ref([])
+    // 获取最新购物车列表action
+    const updateNewList = async () => {
+        const res = await findNewCartListAPI()
+        cartList.value = res.result
+    }
     // 2. 定义action - addCart
-    const addCart = (goods) => {
-        // 添加购物车操作
-        // 已添加过 - count + 1
-        // 没有添加过 - 直接push
-        // 思路: 通过匹配传递过来的商品对象中的skuId能不能在cartList中找到，找到了就是添加过
-        const item = cartList.value.find((item) => goods.skuId === item.skuId)
-        if (item) {
-            // 找到了
-            item.count++
+    const addCart = async (goods) => {
+        const { skuId, count } = goods
+        if (isLogin.value) {
+            // 登录之   后的加入购物车逻辑
+            // 登录之后的加入购车逻辑
+            await insertCartAPI({ skuId, count })
+            updateNewList()
         } else {
-            // 没找到
-            cartList.value.push(goods)
+            // 添加购物车操作
+            // 已添加过 - count + 1
+            // 没有添加过 - 直接push
+            // 思路: 通过匹配传递过来的商品对象中的skuId能不能在cartList中找到，找到了就是添加过
+            const item = cartList.value.find((item) => goods.skuId === item.skuId)
+            if (item) {
+                // 找到了
+                item.count++
+            } else {
+                // 没找到
+                cartList.value.push(goods)
+            }
         }
     }
 
     // 删除购物车
-    const delCart = (skuId) => {
-        // 思路：
-        // 1. 找到要删除项的下标值 - splice
-        // 2. 使用数组的过滤方法 - filter
-        const idx = cartList.value.findIndex((item) => skuId === item)
-        cartList.value.splice(idx, 1)
+    const delCart = async (skuId) => {
+        if (isLogin.value) {
+            // 调用接口实现接口购物车中的删除功能
+            await delCartAPI([skuId])
+            updateNewList()
+        } else {
+            // 思路：
+            // 1. 找到要删除项的下标值 - splice
+            // 2. 使用数组的过滤方法 - filter
+            const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+            cartList.value.splice(idx, 1)
+        }
+    }
+
+    // 清除购物车
+    const clearCart = () => {
+        cartList.value = []
     }
 
     // 单选功能
@@ -62,15 +90,17 @@ export const useCartStore = defineStore('cart', () => {
 
     return {
         cartList,
-        addCart,
-        delCart,
-        isAll,
         allCount,
         allPrice,
+        isAll,
+        selectedCount,
+        selectedPrice,
+        clearCart,
+        addCart,
+        delCart,
         singleCheck,
         allCheck,
-        selectedCount,
-        selectedPrice
+        updateNewList
     }
 }, {
     persist: true,
