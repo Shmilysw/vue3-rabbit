f2334084937319e85af9308ea97f9ad1e0f26e33
shiwei <1828045443@qq.com>
2023-05-29 17:06:46 +0800

    本地购物车-购物车列表-统计计算

--- vue3-rabbit/src/stores/cartStore.js
+++ vue3-rabbit/src/stores/cartStore.js
@@ -51,6 +51,11 @@ export const useCartStore = defineStore('cart', () => {
     // 2. 总价 所以项的count * price之和
     const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
 
+    // 3. 已选择数量
+    const selectedCount = computed(() => cartList.value.filter(item => item.selected).reduce((a, c) => a + c.count, 0))
+    // 4. 已选择商品价格合计
+    const selectedPrice = computed(() => cartList.value.filter(item => item.selected).reduce((a, c) => a + c.count * c.price, 0))
+
     // 是否全选
     const isAll = computed(() => cartList.value.every((item) => item.selected))
 
@@ -63,7 +68,9 @@ export const useCartStore = defineStore('cart', () => {
         allCount,
         allPrice,
         singleCheck,
-        allCheck
+        allCheck,
+        selectedCount,
+        selectedPrice
     }
 }, {
     persist: true,
--- vue3-rabbit/src/views/CartList/index.vue
+++ vue3-rabbit/src/views/CartList/index.vue
@@ -71,7 +71,7 @@ const allCheck = (selected) => {
                                 <p>&yen;{{ i.price }}</p>
                             </td>
                             <td class="tc">
-                                <el-input-number v-model="i.count" />
+                                <el-input-number v-model="i.count" :min="0" />
                             </td>
                             <td class="tc">
                                 <p class="f16 red">&yen;{{ (i.price * i.count).toFixed(2) }}</p>
@@ -102,8 +102,8 @@ const allCheck = (selected) => {
             <!-- 操作栏 -->
             <div class="action">
                 <div class="batch">
-                    共 10 件商品，已选择 2 件，商品合计：
-                    <span class="red">¥ 100 </span>
+                    共 {{ cartStore.allCount }} 件商品，已选择 {{ cartStore.selectedCount }} 件，商品合计：
+                    <span class="red">¥ {{ cartStore.selectedPrice.toFixed(2) }} </span>
                 </div>
                 <div class="total">
                     <el-button size="large" type="primary" @click="$router.push('/checkout')">下单结算</el-button>
