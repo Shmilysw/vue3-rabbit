1bd61a92201a25ececa3870d3af5becd5d573983
shiwei <1828045443@qq.com>
2023-05-29 21:56:10 +0800

    支付结果展示

--- vue3-rabbit/src/router/index.js
+++ vue3-rabbit/src/router/index.js
@@ -11,6 +11,7 @@ import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
+import PayBack from '@/views/Pay/PayBack.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -47,7 +48,11 @@ const router = createRouter({
                 {
                     path: 'pay',
                     component: Pay
-                }
+                },
+                {
+                    path: 'paycallback',
+                    component: PayBack
+                },
             ]
         },
         {
