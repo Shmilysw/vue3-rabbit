ebd9372a4e4c86514d0114a93f4f1f362686f416
shiwei <1828045443@qq.com>
2023-05-29 18:00:34 +0800

    登录时合并购物车

--- vue3-rabbit/src/stores/userStore.js
+++ vue3-rabbit/src/stores/userStore.js
@@ -4,6 +4,7 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { loginAPI } from '@/apis/user'
 import { useCartStore } from './cartStore'
+import { mergeCartAPI } from '@/apis/cart'
 
 export const useUserStore = defineStore('user', () => {
     const cartStore = useCartStore()
@@ -13,6 +14,15 @@ export const useUserStore = defineStore('user', () => {
     const getUserInfo = async ({ account, password }) => {
         const res = await loginAPI({ account, password })
         userInfo.value = res.result
+        // 合并购物车的操作
+        await mergeCartAPI(cartStore.cartList.map(item => {
+            return {
+                skuId: item.skuId,
+                selected: item.selected,
+                count: item.count
+            }
+        }))
+        cartStore.updateNewList()
     }
 
     // 退出时清除用户信息
