e0c02c0190ed36973d8b4faefada3abb051ce525
shiwei <1828045443@qq.com>
2023-06-01 20:32:27 +0800

    会员中心功能理解以及路由配置

--- vue3-rabbit/src/apis/user.js
+++ vue3-rabbit/src/apis/user.js
@@ -11,3 +11,12 @@ export const loginAPI = ({ account, password }) => {
         }
     })
 }
+
+export const getLikeListAPI = ({ limit = 4 }) => {
+    return request({
+        url: '/goods/relevant',
+        params: {
+            limit
+        }
+    })
+}
--- vue3-rabbit/src/router/index.js
+++ vue3-rabbit/src/router/index.js
@@ -12,6 +12,10 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
+import Member from '@/views/Member/index.vue'
+import UserInfo from '@/views/Member/components/UserInfo.vue'
+import UserOrder from '@/views/Member/components/UserOrder.vue'
+
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -53,6 +57,20 @@ const router = createRouter({
                     path: 'paycallback',
                     component: PayBack
                 },
+                {
+                    path: 'member',
+                    component: Member,
+                    children: [
+                        {
+                            path: 'user',
+                            component: UserInfo
+                        },
+                        {
+                            path: 'order',
+                            component: UserOrder
+                        }
+                    ]
+                }
             ]
         },
         {
